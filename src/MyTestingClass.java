d9dea704b1e1c8a9a715f206be4b06d4355e7ad2
quemin2402 <mereyibraim2006@gmail.com>
2024-05-16 15:09:26 +0500

    Added MyTestingClass with detailed comments explaining attributes and hashCode method.

---
+++ src/MyTestingClass.java
@@ -0,0 +1,57 @@
+/**
+ * A class used for testing purposes.
+ */
+public class MyTestingClass {
+    private int attribute1; // First attribute of the class
+    private int attribute2; // Second attribute of the class
+
+    /**
+     * Constructor to create an instance of MyTestingClass with the given attributes.
+     * @param attribute1 The first attribute.
+     * @param attribute2 The second attribute.
+     */
+    public MyTestingClass(int attribute1, int attribute2) {
+        this.attribute1 = attribute1;
+        this.attribute2 = attribute2;
+    }
+
+    /**
+     * Computes a custom hash code for this instance based on its attributes.
+     * The hash code is calculated using a formula that combines the two attributes
+     * with different prime numbers to reduce the likelihood of collisions.
+     * This approach helps in creating a more uniformly distributed hash code.
+     *
+     * @return The hash code of this instance.
+     */
+    @Override
+    public int hashCode() {
+        int prime1 = 31; // First prime number used in hash calculation
+        int prime2 = 37; // Second prime number used in hash calculation
+        return prime1 * attribute1 + prime2 * attribute2;
+    }
+
+    /**
+     * Compares this instance with another object for equality.
+     * @param obj The object to be compared.
+     * @return True if the given object is equal to this instance, false otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true; // Check if both references are the same
+        if (obj == null || getClass() != obj.getClass()) return false; // Check if obj is null or not of the same class
+        MyTestingClass other = (MyTestingClass) obj; // Typecast obj to MyTestingClass
+        return attribute1 == other.attribute1 && attribute2 == other.attribute2; // Compare attributes for equality
+    }
+
+    /**
+     * Provides a string representation of this instance.
+     * @return A string representation of the instance in the format "MyTestingClass {attribute1 = x, attribute2 = y}".
+     */
+    @Override
+    public String toString() {
+        return "MyTestingClass {" +
+                "attribute1 = " + attribute1 +
+                ", attribute2 = " + attribute2 +
+                " }";
+    }
+}
